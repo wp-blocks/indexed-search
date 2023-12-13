a921436873a1de6e577790fb775a92ecfe0be854
John Hooks <bitmachina@outlook.com>
2023-12-12 21:09:24 -0800

    ci: generate test files outside container

--- bin/test-up.sh
+++ bin/test-up.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker compose --file tests/docker-compose.yml --env-file tests/.env up --wait
-docker compose --file tests/docker-compose.yml --env-file tests/.env exec -T wordpress chown www-data:www-data wp-content wp-content/plugins
+docker compose --file tests/docker-compose.yml --env-file tests/.env exec -T wordpress chown -R www-data:www-data wp-content wp-content/plugins wp-content/plugins/indexed-search wp-content/plugins/indexed-search/tests wp-content/plugins/indexed-search/tests/_support wp-content/plugins/indexed-search/tests/_support/_generated wp-content/plugins/indexed-search/tests/_output
 docker compose --file tests/docker-compose.yml --env-file tests/.env exec -T -w /var/www/html/wp-content/plugins/indexed-search wordpress bash bin/wp-install.sh
