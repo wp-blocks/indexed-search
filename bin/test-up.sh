f6430e6e001847a468d22a9b06d57ec0172cbb18
John Hooks <bitmachina@outlook.com>
2023-12-12 20:18:29 -0800

    ci: add wp-cli install script for test container

--- bin/test-up.sh
+++ bin/test-up.sh
@@ -2,3 +2,4 @@
 
 docker compose --file tests/docker-compose.yml --env-file tests/.env up --wait
 docker compose --file tests/docker-compose.yml --env-file tests/.env exec -T wordpress chown www-data:www-data wp-content wp-content/plugins
+docker compose --file tests/docker-compose.yml --env-file tests/.env exec -T -w /var/www/html/wp-content/plugins/indexed-search wordpress bash bin/wp-install.sh
